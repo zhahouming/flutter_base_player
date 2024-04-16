94d4e8b48d12396186d1c1995c2463848d9e46be
zhazhiwen <zhazhiwen@bytedance.com>
2024-04-16 16:39:45 +0800

    fix: 0.0.2

--- inode/lib/inode.dart
+++ inode/lib/inode.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'inode_platform_interface.dart';
 
 class Inode {
-  Future<int?> getInode(String path) {
+  Future<int?> getInode(String path) async {
+    var file = File(path);
+    if (!file.existsSync()) {
+      return null;
+    }
     return InodePlatform.instance.getInode(path);
   }
 }
